b4519d8137fa1d8a1c18a9280ef6fa5804c7e169
Essy Mwangi <sonnieessy@gmail.com>
2020-05-22 15:16:25 +0300

    Initial commit

---
+++ mygallery/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Location(models.Model):
+  name = models.CharField(max_length=50)
+
+
+  @classmethod
+  def get_locations(cls):
+    locations = Location.objects.all()
+    return locations
+
+  def __str__(self):
+    return self.name
+
+  @classmethod
+  def update_location(cls, id, value):
+    cls.objects.filter(id=id).update(image=value)
+
+  def save_location(self):
+    self.save()
+
+  def delete_location(self):
+    self.delete()
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/essie/Documents/moringa-school-projects/moringa-core/Python/Django/IPS/week-1-ip/my-gallery/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
